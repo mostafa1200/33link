63de2a89af9b397889af9ea6ea6348f5b3edb068
mostafa1200 <46429249+mostafa1200@users.noreply.github.com>
2019-04-06 04:14:09 +0200

    Create 33link.js

---
+++ link/dfvfd/33link.js
@@ -0,0 +1,24 @@
+<script type='text/javascript'>
+ //<![CDATA[
+var allahref=document.getElementsByTagName("a");
+for(var c=0;c<allahref.length;c++)
+{
+if(allahref[c].hostname!="https://33link.blogspot.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.facebook.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.youtube.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="plus.google.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.instagram.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="twitter.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="1.bp.blogspot.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="2.bp.blogspot.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="3.bp.blogspot.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="4.bp.blogspot.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.blogger.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="www.linkedin.com" && allahref[c].hostname!="")
+if(allahref[c].hostname!="play.google.com" && allahref[c].hostname!="")
+{
+allahref[c].href="https://33link.blogspot.com/p/blog-page.html?&&url=_"+allahref[c].href;
+}
+}
+ //]]>
+</script>
